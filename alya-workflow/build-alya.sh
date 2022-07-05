ccedafa1b0287654b28c0f1aa26a8ab5b0b1a6af
Arnis Lektauers <arnis.lektauers@rtu.lv>
2022-07-05 10:31:36 +0300

    Update .gitignore and build-alya.sh

--- alya-workflow/build-alya.sh
+++ alya-workflow/build-alya.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 if ! [  -n "$(uname -a | grep Ubuntu)" ]; then
-  module load spack/cmake/3.21.4-gcc-11.2.0
-  module load spack/gcc-11.2.0
+  module load compilers/cmake/3.21.4-gcc-11.2.0
+  module load compilers/gcc-11.2.0
   module load mpi/openmpi-4.1.1
 fi
 
 cd ../alya-raise/build
+rm CMakeCache.txt
 
 cmake ..
 
 make clean
 
 CORES=$(getconf _NPROCESSORS_ONLN)
-echo "Number of CPU/cores: $CORES"
+echo "Number of CPU/cores used for build: $CORES"
 
 make -j$CORES
-make install
+#make install
 
 cp src/alya/alya ../../alya-workflow/bin
 cp src/alya2pos/alya2pos ../../alya-workflow/bin
