de82383baa192d8e2b001d889688ff58631f570f
Arnis Lektauers <arnis.lektauers@rtu.lv>
2023-02-17 14:53:08 +0200

    Update spatial transfomer model

--- ai-model/model_surrogate/spatial_transformer.py
+++ ai-model/model_surrogate/spatial_transformer.py
@@ -3,15 +3,12 @@ import torch
 from torch.nn import Module, Conv2d, Dropout2d, Linear, Sequential, MaxPool2d, ReLU
 import torch.nn.functional as F
 
+from convolutional_network import ConvolutionalNetwork
 
-class SpatialTransformer(Module):
+
+class SpatialTransformer(ConvolutionalNetwork):
     def __init__(self, num_channels=2):
-        super(SpatialTransformer, self).__init__()
-        self.conv1 = Conv2d(1, 10, kernel_size=5)
-        self.conv2 = Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = Dropout2d()
-        self.fc1 = Linear(320, 50)
-        self.fc2 = Linear(50, 10)
+        super(SpatialTransformer, self).__init__(num_channels)
 
         # Spatial transformer localization-network
         self.localization = Sequential(
@@ -24,21 +21,38 @@ class SpatialTransformer(Module):
         )
 
         # Regressor for the 3 * 2 affine matrix
-        self.fc_loc = Sequential(
-            Linear(15360, 32),
+        self.fc_loc_a = Sequential(
+            Linear(10 * 4 * 12, 32),
+            ReLU(True),
+            Linear(32, 3 * 2)
+        )
+
+        self.fc_loc_b = Sequential(
+            Linear(10 * 38 * 17, 32),
             ReLU(True),
             Linear(32, 3 * 2)
         )
 
         # Initialize the weights/bias with identity transformation
-        self.fc_loc[2].weight.data.zero_()
-        self.fc_loc[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
+        self.fc_loc_a[2].weight.data.zero_()
+        self.fc_loc_a[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
 
     # Spatial transformer network forward function
-    def stn(self, x):
+    def stn_a(self, x):
         xs = self.localization(x)
-        xs = xs.view(-1, 15360)
-        theta = self.fc_loc(xs)
+        xs = xs.view(-1, 10 * 4 * 12)
+        theta = self.fc_loc_a(xs)
+        theta = theta.view(-1, 2, 3)
+
+        grid = F.affine_grid(theta, x.size())
+        x = F.grid_sample(x, grid)
+
+        return x
+
+    def stn_b(self, x):
+        xs = self.localization(x)
+        xs = xs.view(-1, 10 * 38 * 17)
+        theta = self.fc_loc_b(xs)
         theta = theta.view(-1, 2, 3)
 
         grid = F.affine_grid(theta, x.size())
@@ -48,13 +62,7 @@ class SpatialTransformer(Module):
 
     def forward(self, xa, xb):
         # transform the input
-        x = self.stn(xa)
+        xa = self.stn_a(xa)
+        xb = self.stn_b(xb)
 
-        # Perform the usual forward pass
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+        return super(SpatialTransformer, self).forward(xa, xb)
