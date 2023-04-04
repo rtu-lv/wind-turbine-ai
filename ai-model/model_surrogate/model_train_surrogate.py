4349281cb37ffdfbbb9c414b843984b60edec7b2
Arnis Lektauers <arnis.lektauers@rtu.lv>
2023-04-04 17:16:31 +0300

    Fix of TensorBoard logging: use epochs instead of steps

--- ai-model/model_surrogate/model_train_surrogate.py
+++ ai-model/model_surrogate/model_train_surrogate.py
@@ -23,6 +23,8 @@ from convolutional_network import ConvolutionalNetwork
 
 TUNING_LOGS_DIR = "tuning_logs"
 
+TUNING_LOGS_DIR = "tuning_logs"
+
 current_dir = os.path.dirname(os.path.realpath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
