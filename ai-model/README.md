f94d9912727bbd5dc441e3973a707bcf03735936
Arnis Lektauers <arnis.lektauers@gmail.com>
2024-04-26 10:45:48 +0300

    Update run script

--- ai-model/README.md
+++ ai-model/README.md
@@ -1,3 +1,5 @@
 # RAISE Wind Turbine AI Model
 
 https://gitlab.com/AlbertPM/raise-toy-models/-/wikis/home
+
+conda install -c conda-forge torchmetrics
--- ai-model/model_surrogate/run_train_surrogate_rtu.sh
+++ ai-model/model_surrogate/run_train_surrogate_rtu.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
-python3 model_train_surrogate.py --model cnnA.pth --data surrDS.bin --epochs 10 --trials 10 \
---num_cpus $PBS_NUM_PPN --num_gpus 1 --db_path /mnt/home/arnisl/raise/wind-turbine-ai/data
+#python3 model_train_surrogate.py --model cnnA.pth --data surrDS.bin --epochs 10 --trials 10 \
+#--num_cpus $PBS_NUM_PPN --num_gpus 1 --db_path /mnt/home/arnisl/raise/wind-turbine-ai/data
+
+eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
+conda activate raise
+
+cd ~/projects/raise/wind-turbine-ai/ai-model/model_surrogate
+echo "Script executed from: ${PWD}"
+ 
+python3 -u model_train_surrogate.py --model cnnA.pth --data surrDS2.bin --epochs 20 --trials 1 --db_path /home/raise/projects/raise/wind-turbine-ai/data/timeseries
+
+echo "*** finished ****""
